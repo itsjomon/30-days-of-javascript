3eba46372c1f98cda4c5dd6abd2de5141764fea7
Jomon <202098873+itsjomon@users.noreply.github.com>
2025-10-27 11:10:45 +0530

    Add solution for Event Emitter

---
+++ classes/2694.js
@@ -0,0 +1,55 @@
+// 2694. Event Emitter
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+
+        this.events[eventName].push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = this.events[eventName].indexOf(callback);
+                if (index !== -1) {
+                    this.events[eventName].splice(index, 1);
+                }
+                return undefined;
+            }
+        };
+    }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        if (!this.events[eventName]) {
+            return [];
+        }
+
+        return this.events[eventName].map(cb => cb(...args));
+    }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
